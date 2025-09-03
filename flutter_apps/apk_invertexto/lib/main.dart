f8bd5e58200376be842a5c579d0dfe3edc9b44ae
Herich <herichop013@gmail.com>
2025-09-03 09:57:59 -0300

    aula 03/09 api invertexto

---
+++ flutter_apps/apk_invertexto/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:apk_invertexto/view/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
---
+++ flutter_apps/apk_invertexto/lib/view/por_extenso_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class PorExtensoPage extends StatefulWidget {
+  const PorExtensoPage({super.key});
+
+  @override
+  State<PorExtensoPage> createState() => _PorExtensoPageState();
+}
+
+class _PorExtensoPageState extends State<PorExtensoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/imgs/image.png',
+              fit: BoxFit.contain,
+              height: 40,
+            ),
+          ],
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+        ),
+      ),
+      backgroundColor: Colors.black,
+      body: Padding(padding: EdgeInsets.all(10), child: Column()),
+    );
+  }
+}
